089fb9f850093734b2f11df8aab46e9da293f52c
Kymani Stephens <kymanist00@gmail.com>
2023-05-12 12:38:59 -0700

    add properties for order class

--- BakeryTracker/Models/Order.cs
+++ BakeryTracker/Models/Order.cs
@@ -4,6 +4,14 @@ namespace BakeryTracker.Models
 {
     public class Order
     {
-        
+        public string Details { get; set; }
+        public int Id { get; }
+        private static List<Order> _instances = new List<Order> { };
+        private Order(string details )
+        {
+            Details = details;
+            Id = _instances.Count; 
+
+        }
     }
 }
