eaa9d460be0d21d866cf15c5e1c394ba56b50591
Kymani Stephens <kymanist00@gmail.com>
2023-05-12 13:27:26 -0700

    add controllers

---
+++ BakeryTracker/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+
+namespace BakeryTracker.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        // [HttpGet("/")]
+        // public ActionResult Index()
+        // {
+        //     return View(); 
+        // }
+
+    }
+}
---
+++ BakeryTracker/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+
+namespace BakeryTracker.Controllers
+{
+    public class VendorController : Controller
+    {
+
+        // [HttpGet("/")]
+        // public ActionResult Index()
+        // {
+        //     return View(); 
+        // }
+
+    }
+}
