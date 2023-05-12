ebd44ad86db83b6b94ca48ffd359121b665cfa0b
Kymani Stephens <kymanist00@gmail.com>
2023-05-12 12:09:48 -0700

    finish properties for Vendor model

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -6,26 +6,34 @@ namespace BakeryTracker.Models
 {
     public class Vendor
     {
-
         /*  This private list will hold all instances of the "Vendor"
             class that get created */
         private static List<Vendor> _instances = new List<Vendor> { };
-        /*  This list will hold all "Order" objects that belong to each instance
+        /*  This list will hold all "Order" objects that are created
+            and belong to the instance
             of the Vendor class */
         public List<Order> Order { get; set; }
         //  Name for each vendor created
         public string Name { get; set; }
         //  Description of each vendor created
         public string About { get; set; }
+        /*  This is a "read-only" property for each "Vendor" instance
+            can have a unique ID */
+        public int Id { get; } 
 
-        /*  This is the constructor that builds the instance of Vendor 
+        /*  This is the constructor that builds each instance of a "Vendor" object
             (blueprint for Vendor class) */ 
         public Vendor(string vendorName, string vendorDescription)
         {
             Name = vendorName;
-            About = vendorDescription;
+            About = vendorDescription; 
+            /*  Adds each created instance of "Vendor" object to list named
+                "_instances" ( Which is defined above ) */ 
+            _instances.Add(this);
+            /*  This will set the value of the Id property for each "Vendor" created 
+                to a unique value (The current count of _instances list) */
+            Id = _instances.Count;
 
         }
-
     }
 }
