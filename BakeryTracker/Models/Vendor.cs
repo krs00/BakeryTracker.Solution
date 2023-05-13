50da74840879c979cbe688ded539d1aab0b9709e
Kymani Stephens <kymanist00@gmail.com>
2023-05-13 12:16:44 -0700

    add searchId vendor method

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -46,5 +46,10 @@ namespace BakeryTracker.Models
             _instances.Clear();
         }
 
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId-1]; 
+        }
+
     }
 }
