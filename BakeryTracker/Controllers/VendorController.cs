4479f9042443462825085a3b0b68c4d4935595e0
Kymani Stephens <kymanist00@gmail.com>
2023-05-14 19:30:28 -0700

    add route for individual vendor page

--- BakeryTracker/Controllers/VendorController.cs
+++ BakeryTracker/Controllers/VendorController.cs
@@ -24,8 +24,19 @@ namespace BakeryTracker.Controllers
         [HttpPost("/vendor")]
         public ActionResult Create(string vendorName, string vendorDescription)
         {
-            Vendor newVendor = new Vendor(vendorName, vendorDescription); 
-            return RedirectToAction("Index"); 
+            Vendor newVendor = new Vendor(vendorName, vendorDescription);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/vendor/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders; 
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
         }
 
     }
